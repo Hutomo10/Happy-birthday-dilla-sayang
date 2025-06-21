2244df2cc0f5ae89525cf73fec69b9ee24ab65de
hutomo <159711961+Hutomo10@users.noreply.github.com>
2025-06-21 17:12:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    texts: [
+        "SEMOGA BANYAK REZEKINYA",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "BANYAK KEBAHAGIAAN DIHIDUP KAMU",  // 同上...
+        "LANCAR SEMUA URUSAN KAMU KEDEPANNYA",
+        "JADI PEREMPUAN YANG LEBIH BAIK LGI Y KEDEPANNYA",
+        "YANG RAJIN BELAJARNYA",
+        "MAAF KLO AKU SUKA BIKIN SAKIT KAMU TERUS WKWKKW",
+        "AKU SENANG KOK BISA KETEMU SAMA KAMU",
+        "UDAH 22 TAHUN INGET HARUS LEBIH SERIUS SAMA HIDUPMU",
+        "AKU SENANG KLO KAMU SEKARANG BANYAK IKUT KEGIATAN POSITIF",
+        "G USAH SEDIH SEDIH LGI YA CANTIK!",
+        "MAKASIH BUAT SEMUANYA DAN MAAF Y KETEMU SAMA ORANG KERAS KEPALA BANGET KEK AKU",
+    ],
+    /*
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        
+    },
+  
+    desc: {
+        turn_on: "DITEKAN YA",
+        play: "DITEKAN LGI:)",
+        bannar_coming: "SEKALI LGI HEHE",
+        balloons_flying: "SELAMAT ULANG TAHUN CANTIK",
+        cake_fadein: "UDAH TUA YA SEKARANG",
+        light_candle: "DITEKAN LGI YA",
+        wish_message: "Ditiup Kuenya Cantik",
+        story: "ada pesan khusus buat kamu",
+    }
+};
